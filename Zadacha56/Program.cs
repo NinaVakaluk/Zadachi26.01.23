f3c56fe84a5bb9d853db95a77174e5130de56c01
Nina <you@example.com>
2023-01-29 20:47:30 +0300

    Решение задач 54,56,58

---
+++ Zadacha56/Program.cs
@@ -0,0 +1,110 @@
+﻿/*Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+int[,] array = GetArray(4, 4, 0, 10);
+PrintArray(array);
+int min = Int32.MaxValue;
+int Line = 0;
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+
+
+        Console.WriteLine();
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum = sum + array[i, j];
+    }
+    if (sum < min)
+    {
+        min = sum;
+        Line = i + 1;
+    }
+}
+Console.WriteLine($"номер строки с наименьшей суммой элементов => {Line}, с суммой элементов равной : {min} " );
+
+
+
+
+
+/*Console.WriteLine("введите размер квадратного массива");
+int massVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[massVol, massVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int minsum = Int32.MaxValue;
+int indexLine = 0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum = sum + numbers[i, j];
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine = i + 1;
+    }
+}
+
+Console.WriteLine("строка с наименьшей суммой елементов под номером: " + (indexLine) + ", с суммой елементов равной: " + (minsum));
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}*/
---
+++ Zadacha58/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+
+int[,] matrix1 = GetArray(2, 2, 0, 10);
+int[,] matrix2 = GetArray(2, 2, 0, 10);
+Console.WriteLine("Матрица 1:");
+PrintArray(matrix1);
+Console.WriteLine();
+Console.WriteLine("Матрица 2:");
+PrintArray(matrix2);
+Console.WriteLine("Результирующая матрица:");
+int[,] newmatrix = Multiplication(matrix1,matrix2);
+PrintArray(newmatrix);
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+
+
+        Console.WriteLine();
+    }
+}
+
+int[,] Multiplication(int[,] A, int[,] B)
+        {
+            
+            int[,] C = new int[A.GetLength(0), B.GetLength(1)];
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    for (int k = 0; k < B.GetLength(0); k++)
+                    {
+                        C[i,j] += A[i,k] * B[k,j];
+                    }
+                }
+            }
+            return C;
+        }
